5ed61c2941b9d9efae6f18f0ac803a480f10daea
anantakumari04 <kumariananta01@gmail.com>
2025-10-05 23:22:21 +0530

    updated

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const jobsRouter = require('./routes/jobs');
 
 
 const app = express();
-app.use(cors({
-  origin: '*',
-}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({extended:true}));
